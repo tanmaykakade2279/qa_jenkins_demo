f954a7b89287818ac297cc760cd87e9f8e96a41e
tanmaykakade2279 <56625360+tanmaykakade2279@users.noreply.github.com>
2020-07-15 12:06:44 +0530

    Updated script

--- qa_shell_script1.sh
+++ qa_shell_script1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION_FILE=$1
+#VERSION_FILE=$1
 echo "This is shell script for Tester Job1"
 pwd
 date
-source $VERSION_FILE
-echo "Build version is:$BUILD_VERSION"
+#source $VERSION_FILE
+#echo "Build version is:$BUILD_VERSION"
